24c0e9802f0e36e876d96354fb3e9012a8b80650
shanafzal060 <shanafzal060@gmail.com>
2022-10-21 17:58:03 +0500

    form validation in vue js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-validation></form-validation>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import formValidation from './components/formValidation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    formValidation
   }
 }
 </script>
---
+++ src/components/formValidation.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>form validation demonstration</h1>
+        <p v-if="error.length">
+            <b>Please correct the following errors</b>
+            <ul>
+                <li>
+                    <h4 v-for="err in error" :key="err.id">
+                        {{err}}
+                    </h4>
+                </li>
+            </ul>
+        </p>
+
+        <form v-on:submit="login">
+            <input type="text" name="" id="" placeholder="Name" v-bind="name"> <br><br>
+            <input type="password" name="" id="" placeholder="Password" v-bind="password"><br><br>
+            <button type="submit">submit </button>
+        </form>
+
+    </div>
+</template>
+<script>
+export default {
+
+    name:'formValidation',
+
+    data(){
+        return {
+            error:[],
+            name:null,
+            password:null
+        }
+    },
+
+    methods:{
+        login(e){
+            
+            if(this.name && this.password){
+                console.warn('no errors')
+            }
+            if( this.name ){
+                this.error.push("plz enter the name");
+            }
+            if( this.password  ){
+                this.error.push("plz enter the password");
+            }
+            console.warn('hello' ,this.error)
+            e.preventDefault()
+        }
+    }
+
+}
+</script>
